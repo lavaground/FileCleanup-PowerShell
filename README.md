ba669e7c8314215300a84d87af8f162631f90c6e
lavaground <lavaground@gmail.com>
2021-05-27 14:24:11 +0200

    Inital release

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # MKVCleanup-PowerShell
 A simple tool to remove tiltes, tags and subtiltes from multiple mkv containers and regex-rename afterwards
+
+# Requirements
+- [MKVToolNix](https://mkvtoolnix.download/downloads.html#windows) - Download and install or use Winget: ```winget install mkvtoolnix ```
+
+# Setup
+Edit Line 1: "PATH\TO\YOUR\CLEANUP\FOLDER" accordingly.
+
+# Usage
+Run the script via **Right-Click -> "Run with PowerShell"** or **PowerShell** or **CMD**
+
+# Notes
+- This script will delete all titles, tags and subtitles from the files in a specified folder and regex-rename them.
+- **CAUTION**: Do not use this on your movies/tv folder directly, it might remove desired subtitles.
---
+++ TV_Cleanup.ps1
@@ -0,0 +1,54 @@
+﻿$rootfolder = "PATH\TO\YOUR\CLEANUP\FOLDER\"
+"==============================================================================================================
+
+Enumerating all MKVs under $rootfolder
+
+==============================================================================================================
+"
+
+$files = gci $rootfolder -Recurse -Include *.mkv
+if($files.Count -gt 0) {
+ $files | ForEach-Object {
+  mkvpropedit.exe "$_" -d title
+  mkvpropedit.exe "$_" -e track:v1 -d name
+  mkvpropedit.exe "$_" -e track:a1 -d name
+  mkvpropedit.exe "$_" -e track:s1 -d name
+  Write-Output "
+==============================================================================================================
+
+Title cleaning in file $_.Name is done.
+
+==============================================================================================================
+
+Searching for subtiltes in $_.
+
+==============================================================================================================
+"
+  $file = mkvmerge -i $_
+  $hassub = $file[-1].Contains("subtitles")
+  if($hassub) {
+   Write-Output "$_ has subtitles."
+   $NewName = $_.FullName -replace "(^.*S[\d]+E[\d]+).*", "`$1.720p.x265.mkv"
+   mkvmerge -q -o "$NewName" -S $_
+   if($LASTEXITCODE -eq 1) {
+    Write-Output "Warnings/errors generated during remuxing, original file not deleted"
+   }else{
+    remove-Item $_
+    Write-Output "Successfully remuxed to $NewName, original file deleted"
+   }
+  }else{
+   Write-Output "$_ has no subtitles, renaming file."
+   $NewName = $_.FullName -replace "(^.*S[\d]+E[\d]+).*", "`$1.720p.x265.mkv"
+   Rename-Item -Path $_.FullName -NewName $NewName
+  }
+ }
+}else{
+ Write-Output "No files found."
+}
+Write-Output "
+==============================================================================================================
+
+Operation completed.
+
+==============================================================================================================
+"

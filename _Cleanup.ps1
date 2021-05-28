8ccfae53d9f79871d033747d8e4e2e7350cd6395
lavaground <lavaground@gmail.com>
2021-05-28 02:17:59 +0200

    Take params and handle Write-Output nicer

---
+++ _Cleanup.ps1
@@ -0,0 +1,66 @@
+﻿param (
+  [string]$SearchFor = "(^.*S[\d]+E[\d]+).*", 
+  [string]$ReplaceWith = "`$1.720p.x265.mkv",
+  [string]$FileExtension = "*.mkv",
+  [string]$RootFolder = "PATH\TO\YOUR\CLEANUP\FOLDER\"
+)
+
+$SeperatorString = "`n==============================================================================================================`n"
+$RemuxErrorString = "Warnings/errors generated during remuxing, original file not deleted"
+$NoRenamesString = "Nothing to rename"
+$NoFilesString = "No files found."
+$CompletedString = "Operation completed."
+
+Write-Output $SeperatorString
+Write-Output "Enumerating all files in $RootFolder"
+Write-Output $SeperatorString
+$Files = Get-ChildItem $RootFolder -Recurse -Include $FileExtension
+
+if ($Files.Count -gt 0) {
+  $Files | ForEach-Object {
+    Write-Output "Cleaning titles in file $_."
+    Write-Output $SeperatorString
+    mkvpropedit.exe "$_" -d title
+    mkvpropedit.exe "$_" -e track:v1 -d name
+    mkvpropedit.exe "$_" -e track:a1 -d name
+    mkvpropedit.exe "$_" -e track:s1 -d name
+    Write-Output $SeperatorString
+    Write-Output "Title cleaning in file $_ is done."
+    Write-Output $SeperatorString
+    Write-Output "Searching for subtiltes in $_."
+    Write-Output $SeperatorString
+    $File = mkvmerge -i $_
+    $HasSub = $File[-1].Contains("subtitles")
+    if ($HasSub) {
+      Write-Output "$_ has subtitles."
+      Write-Output $SeperatorString
+      $NewName = $_.FullName -replace $SearchFor, $ReplaceWith
+      mkvmerge -q -o "$NewName" -S $_
+      if ($LASTEXITCODE -eq 1) {
+        Write-Output $RemuxErrorString
+      }
+      else {
+        remove-Item $_
+        Write-Output "Successfully remuxed to $NewName, original file deleted"
+      }
+    }
+    else {
+      Write-Output "$_ has no subtitles, renaming file."
+      Write-Output $SeperatorString
+      $NewName = $_.FullName -replace $SearchFor, $ReplaceWith
+      if ($_.FullName -ne $NewName) {
+        Rename-Item -Path $_.FullName -NewName $NewName
+      }
+      else {
+        Write-Output $NoRenamesString
+      }
+    }
+  }
+}
+else {
+  Write-Output $NoFilesString
+}
+
+Write-Output $SeperatorString
+Write-Output $CompletedString
+Write-Output $SeperatorString
